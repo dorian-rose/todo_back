0d34934eecf0d0067327ae9a3bebd789283f97e6
dorian-rose <dorianrkelly@gmail.com>
2023-08-01 18:38:22 +0200

    routes and controllers added for update and delete of the CRUD

--- routers/taskRouters.js
+++ routers/taskRouters.js
@@ -2,12 +2,13 @@ const express = require("express")
 const router = express.Router();
 const { check } = require("express-validator");
 const { validateInputs } = require("../middleware/validateInputs");
-const { getTasks, getTaskById, getTaskByUid, createTask } = require("../controllers/taskControllers")
+const { getTasks, getTaskById, getTaskByUid, createTask, updateTask, deleteTask } = require("../controllers/taskControllers")
 
 router.post("/", [
     check("uid", "The user ID is obligatory").not().isEmpty(),
     check("todo", "The task name is obligatory").not().isEmpty(),
     check("description", "The task description is obligatory").not().isEmpty(),
+    check("done", "The task status is obligatory").not().isEmpty(),
     validateInputs,
 ], createTask)
 
@@ -17,5 +18,15 @@ router.get("/", getTasks)
 router.get("/id/:id", getTaskById)
 
 router.get("/user/:uid", getTaskByUid)
+
+router.put("/", [
+    check("uid", "The user ID is obligatory").not().isEmpty(),
+    check("todo", "The task name is obligatory").not().isEmpty(),
+    check("description", "The task description is obligatory").not().isEmpty(),
+    check("done", "The task status is obligatory").not().isEmpty(),
+    validateInputs,
+], updateTask)
+
+router.delete("/", deleteTask)
 
 module.exports = router;
